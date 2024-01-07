278220c8c6972c1d9423031c98638945640a9a34
fluffy <fluffy@fluffysnowman.dev>
2024-01-08 00:13:51 +0530

    added more commands + better UI and clear option

--- main.go
+++ main.go
@@ -42,6 +42,10 @@ func main() {
 		log.Panicln(err)
 	}
 
+	if err := g.SetKeybinding("right", 'c', gocui.ModNone, clearMiddlePane); err != nil {
+			log.Panicln(err)
+	}
+
 	if err := g.SetKeybinding("", 'h', gocui.ModNone, switchToView("left")); err != nil {
 		log.Panicln(err)
 	}
@@ -65,6 +69,36 @@ func main() {
 
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
+if v, err := g.SetView("left", 0, 0, maxX/4, maxY-1); err != nil {
+    if err != gocui.ErrUnknownView {
+        return err
+    }
+    v.Title = "Left"
+    v.Wrap = true
+    v.Editable = false
+    v.Highlight = true
+    v.SelBgColor = gocui.ColorGreen
+    v.SelFgColor = gocui.ColorBlack
+    for _, command := range commands {
+        fmt.Fprintln(v, command.Name)
+    }
+    if _, err := g.SetCurrentView("left"); err != nil {
+        return err
+    }
+}
+
+if v, err := g.SetView("middle", maxX/4+1, 0, 3*maxX/4, maxY-1); err != nil {
+    if err != gocui.ErrUnknownView {
+        return err
+    }
+    v.Title = "Middle"
+    v.Wrap = true
+    v.Autoscroll = true
+    v.Highlight = true
+    v.SelBgColor = gocui.ColorGreen
+    v.SelFgColor = gocui.ColorBlack
+}
+
 if v, err := g.SetView("right", 3*maxX/4+1, 0, maxX-1, maxY-1); err != nil {
     if err != gocui.ErrUnknownView {
         return err
@@ -72,9 +106,9 @@ if v, err := g.SetView("right", 3*maxX/4+1, 0, maxX-1, maxY-1); err != nil {
     v.Title = "Right"
     v.Wrap = true
     v.Editable = false
-    v.Highlight = true // Add this line
-    v.SelBgColor = gocui.ColorGreen // Add this line
-    v.SelFgColor = gocui.ColorBlack // Add this line
+    v.Highlight = true
+    v.SelBgColor = gocui.ColorGreen
+    v.SelFgColor = gocui.ColorBlack
     for _, command := range commands {
         fmt.Fprintln(v, command.Name)
     }
@@ -83,51 +117,36 @@ if v, err := g.SetView("right", 3*maxX/4+1, 0, maxX-1, maxY-1); err != nil {
     }
 }
 
-	if v, err := g.SetView("middle", maxX/4+1, 0, 3*maxX/4, maxY-1); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Title = "Middle"
-		v.Wrap = true
-		v.Autoscroll = true
-	}
-
-	if v, err := g.SetView("right", 3*maxX/4+1, 0, maxX-1, maxY-1); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Title = "Right"
-		v.Wrap = true
-		v.Editable = false
-		for _, command := range commands {
-			fmt.Fprintln(v, command.Name)
-		}
-		if _, err := g.SetCurrentView("right"); err != nil {
-			return err
-		}
-	}
-
 	return nil
 }
 
 func executeCommand(g *gocui.Gui, v *gocui.View) error {
-	middleView, err := g.View("middle")
-	if err != nil {
-		return err
-	}
-	_, cy := v.Cursor()
-	command := commands[cy].Cmd
-	middleView.FgColor = gocui.ColorGreen
-	fmt.Fprintln(middleView, "$ "+command)
-	middleView.FgColor = gocui.ColorDefault
-	cmd := exec.Command("/bin/sh", "-c", command)
-	output, err := cmd.Output()
-	if err != nil {
-		return err
-	}
-	fmt.Fprintln(middleView, string(output))
-	fmt.Fprintln(middleView, "\n")
-	return nil
+    middleView, err := g.View("middle")
+    if err != nil {
+        return err
+    }
+    _, cy := v.Cursor()
+    command := commands[cy].Cmd
+    fmt.Fprintln(middleView, "\033[32m$ "+command+"\033[0m") // Add color escape codes
+    cmd := exec.Command("/bin/sh", "-c", command)
+    output, err := cmd.Output()
+    if err != nil {
+        return err
+    }
+    fmt.Fprintln(middleView, string(output))
+    fmt.Fprintln(middleView, "\033[33m--------------------------------------------------\033[0m") // Add horizontal line
+    return nil
+}
+
+func clearMiddlePane(g *gocui.Gui, v *gocui.View) error {
+    middleView, err := g.View("middle")
+    if err != nil {
+        return err
+    }
+    middleView.Clear()
+    middleView.SetCursor(0, 0)
+    middleView.SetOrigin(0, 0)
+    return nil
 }
 
 func switchToView(viewName string) func(g *gocui.Gui, v *gocui.View) error {
