0731c74bdf4af32ecf04fa3b8f6a9b419f10b405
fluffy <fluffy@fluffysnowman.dev>
2024-01-08 00:23:23 +0530

    category based commands sorta working but very glitchy

--- main.go
+++ main.go
@@ -13,13 +13,41 @@ type Command struct {
 	Cmd  string
 }
 
-var commands = []Command{
-	{"List files", "ls"},
-	{"pwd", "pwd"},
-	{"Show Date and Time", "date"},
-	{"Neofetch", "neofetch"},
-	{"CPUs", "nproc"},
+// the old ones 
+
+// var commands = []Command{
+// 	{"List files", "ls"},
+// 	{"pwd", "pwd"},
+// 	{"Show Date and Time", "date"},
+// 	{"Neofetch", "neofetch"},
+// 	{"CPUs", "nproc"},
+// }
+
+var commandGroups = map[string][]Command{
+		"general": {
+				{"List files", "ls"},
+				{"pwd", "pwd"},
+				{"Show Date and Time", "date"},
+				{"Neofetch", "neofetch"},
+				{"CPUs", "nproc"},
+		},
+    "apache": {
+        {"Start Apache", "sudo systemctl start apache2"},
+        {"Stop Apache", "sudo systemctl stop apache2"},
+    },
+    "docker": {
+        {"Start Docker", "sudo systemctl start docker.service docker.socket"},
+        {"Stop Docker", "sudo systemctl stop docker.service docker.socket"},
+    },
+		"git": {
+				{"Git Status", "git status"},
+				{"Git Add", "git add ."},
+				{"Git Push", "git push"},
+				{"Git Pull", "git pull"},
+		},
 }
+
+var selectedGroup = "general" // default which is glitchy for some reasont
 
 func main() {
 	g, err := gocui.NewGui(gocui.OutputNormal)
@@ -68,8 +96,10 @@ func main() {
 }
 
 func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-if v, err := g.SetView("left", 0, 0, maxX/4, maxY-1); err != nil {
+
+    maxX, maxY := g.Size()
+    if v, err := g.SetView("left", 0, 0, maxX/4, maxY-1); err != nil {
+
     if err != gocui.ErrUnknownView {
         return err
     }
@@ -79,9 +109,18 @@ if v, err := g.SetView("left", 0, 0, maxX/4, maxY-1); err != nil {
     v.Highlight = true
     v.SelBgColor = gocui.ColorGreen
     v.SelFgColor = gocui.ColorBlack
-    for _, command := range commands {
-        fmt.Fprintln(v, command.Name)
-    }
+
+		// new one
+		for group := range commandGroups {
+			fmt.Fprintln(v, group)
+		}
+
+		// old one
+
+    // for _, command := range commands {
+    //     fmt.Fprintln(v, command.Name)
+    // }
+
     if _, err := g.SetCurrentView("left"); err != nil {
         return err
     }
@@ -99,7 +138,8 @@ if v, err := g.SetView("middle", maxX/4+1, 0, 3*maxX/4, maxY-1); err != nil {
     v.SelFgColor = gocui.ColorBlack
 }
 
-if v, err := g.SetView("right", 3*maxX/4+1, 0, maxX-1, maxY-1); err != nil {
+    if v, err := g.SetView("right", 3*maxX/4+1, 0, maxX-1, maxY-1); err != nil {
+
     if err != gocui.ErrUnknownView {
         return err
     }
@@ -109,9 +149,9 @@ if v, err := g.SetView("right", 3*maxX/4+1, 0, maxX-1, maxY-1); err != nil {
     v.Highlight = true
     v.SelBgColor = gocui.ColorGreen
     v.SelFgColor = gocui.ColorBlack
-    for _, command := range commands {
-        fmt.Fprintln(v, command.Name)
-    }
+		for _, command := range commandGroups[selectedGroup] {
+			fmt.Fprintln(v, command.Name)
+		}
     if _, err := g.SetCurrentView("right"); err != nil {
         return err
     }
@@ -125,8 +165,8 @@ func executeCommand(g *gocui.Gui, v *gocui.View) error {
     if err != nil {
         return err
     }
-    _, cy := v.Cursor()
-    command := commands[cy].Cmd
+		_, cy := v.Cursor()
+    command := commandGroups[selectedGroup][cy].Cmd
     fmt.Fprintln(middleView, "\033[32m$ "+command+"\033[0m") // Add color escape codes
     cmd := exec.Command("/bin/sh", "-c", command)
     output, err := cmd.Output()
@@ -150,10 +190,14 @@ func clearMiddlePane(g *gocui.Gui, v *gocui.View) error {
 }
 
 func switchToView(viewName string) func(g *gocui.Gui, v *gocui.View) error {
-	return func(g *gocui.Gui, v *gocui.View) error {
-		_, err := g.SetCurrentView(viewName)
-		return err
-	}
+    return func(g *gocui.Gui, v *gocui.View) error {
+        if viewName == "right" {
+            _, cy := v.Cursor()
+            selectedGroup = v.BufferLines()[cy]
+        }
+        _, err := g.SetCurrentView(viewName)
+        return err
+    }
 }
 
 func moveCursorDown(g *gocui.Gui, v *gocui.View) error {
